8c780f5a0a8718e02a470f32e50b45040b5b00cb
TCCFCASTLE123 <clcastle00@gmail.com>
2026-01-13 14:41:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ const allowedOrigins = [
   "http://localhost:5000",
   "https://castle-consulting-firm-messaging.onrender.com",
 ];
+app.use((req, res, next) => {
+  console.log("➡️", req.method, req.url);
+  next();
+});
 
 app.use(
   cors({
@@ -115,3 +119,4 @@ const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
