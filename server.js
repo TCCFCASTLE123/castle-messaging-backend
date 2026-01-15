284430efa79caa4a5140507b90579d92d9bfe247
TCCFCASTLE123 <clcastle00@gmail.com>
2026-01-15 14:33:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const messageRoutes = require("./routes/messages");
 const clientRoutes = require("./routes/clients");
 const statusRoutes = require("./routes/statuses");
 const templateRoutes = require("./routes/templates");
-const scheduledMEessagesRoutes = require("./routes/scheduledMEssages"); // <-- confirm filename casing!
+const scheduledMessagesRoutes = require("./routes/scheduledMEssages"); // <-- confirm filename casing!
 const twilioRoutes = require("./routes/twilio");
 const sheetsWebhookRoutes = require("./routes/sheetsWebhook");
 const internalRoutes = require("./routes/internal"); // ✅ NEW
@@ -132,5 +132,6 @@ const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
