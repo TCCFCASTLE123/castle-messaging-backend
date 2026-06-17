3c7e5a82f95839c9919e2df6c1f551c73e4151d5
TCCFCASTLE123 <clcastle00@gmail.com>
2026-06-17 11:12:05 -0700

    Update createMarti.js

--- createMarti.js
+++ createMarti.js
@@ -6,13 +6,13 @@ const password = 'password123';
 
 bcrypt.hash(password, 10).then(hashedPassword => {
   db.run(
-    'INSERT INTO users (username, password, role) VALUES (?, ?, ?)',
+    'INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)',
     [username, hashedPassword, 'user'],
     function (err) {
       if (err) {
         console.error('Error creating user:', err.message);
       } else {
-        console.log('User created! Username: marti, Password: password123');
+        console.log('User created! Username: marti');
       }
       process.exit();
     }
